3eff22074f9d84df41e6a88783bec0e7d94b4371
Asus <asus@debian>
2018-03-03 21:59:15 +0300

    Выделение изображения работает, но изображение выводится в другом окне

--- main.cpp
+++ main.cpp
@@ -5,67 +5,75 @@
 
 IplImage* image = 0;
 IplImage* src = 0;
+int x_sel, y_sel, width, height;
 
-// рисуем целеуказатель
-void drawTarget(IplImage* img, int x, int y, int radius)
+// рисуем область выделения
+void drawSelected(IplImage* img, int x, int y, int w, int h, int add_value = 200)
 {
-        cvCircle(img,cvPoint(x, y),radius,CV_RGB(250,0,0),1,8);
-        cvLine(img, cvPoint(x-radius/2, y-radius/2), cvPoint(x+radius/2, y+radius/2),CV_RGB(250,0,0),1,8);
-        cvLine(img, cvPoint(x-radius/2, y+radius/2), cvPoint(x+radius/2, y-radius/2),CV_RGB(250,0,0),1,8);
+    cvSetImageROI(img, cvRect(x, y, w, h));
+    cvAddS(img, cvScalar(add_value), image);
 }
 
 // обработчик событий от мышки
 void myMouseCallback( int event, int x, int y, int flags, void* param )
 {
-        IplImage* img = (IplImage*) param;
+    IplImage* img = (IplImage*) param;
 
-        switch( event ){
-                case CV_EVENT_MOUSEMOVE:
-                        break;
+    switch( event )
+    {
+    case CV_EVENT_MOUSEMOVE:
+        break;
 
-                case CV_EVENT_LBUTTONDOWN:
-                        printf("%d x %d\n", x, y);
-                        drawTarget(img, x, y, 10);
-                        break;
+    case CV_EVENT_LBUTTONDOWN:
+        x_sel = x;
+        y_sel = y;
+        break;
 
-                case CV_EVENT_LBUTTONUP:
-                        break;
-        }
+    case CV_EVENT_LBUTTONUP:
+        width = x - x_sel;
+        height = y - y_sel;
+        drawSelected(img, x_sel, y_sel, width, height);
+        cvResetImageROI(image);
+        cvShowImage("ROI", image);
+        break;
+    }
 }
 
 int main(int argc, char* argv[])
 {
-        // имя картинки задаётся первым параметром
-        const char* filename = argc == 2 ? argv[1] : "Image0.jpg";
-        // получаем картинку
-        image = cvLoadImage(filename,1);
-        // клонируем картинку
-        src = cvCloneImage(image);
+    // имя картинки задаётся первым параметром
+    const char* filename = argc == 2 ? argv[1] : "Image0.jpg";
+    // получаем картинку
+    image = cvLoadImage(filename,1);
+    // клонируем картинку
+    src = cvCloneImage(image);
 
-        printf("[i] image: %s\n", filename);
-        assert( src != 0 );
+    printf("[i] image: %s\n", filename);
+    assert( src != 0 );
 
-        // окно для отображения картинки
-        cvNamedWindow("original",CV_WINDOW_AUTOSIZE);
+    // окно для отображения картинки
+    cvNamedWindow("original",CV_WINDOW_AUTOSIZE);
 
-        // вешаем обработчик мышки
-        cvSetMouseCallback( "original", myMouseCallback, (void*) image);
+    // вешаем обработчик мышки
+    cvSetMouseCallback( "original", myMouseCallback, (void*) image);
 
-        while(1){
-                // показываем картинку
-                cvCopyImage( image, src );
-                cvShowImage( "original", src );
+    while(1)
+    {
+        // показываем картинку
+        cvCopyImage( image, src );
+        cvShowImage( "original", src );
 
-                char c = cvWaitKey(33);
-                if (c == 27) { // если нажата ESC - выходим
-                        break;
-                }
+        char c = cvWaitKey(33);
+        if (c == 27)   // если нажата ESC - выходим
+        {
+            break;
         }
+    }
 
-        // освобождаем ресурсы
-        cvReleaseImage(&image);
-        cvReleaseImage(&src);
-        // удаляем окно
-        cvDestroyWindow("original");
-        return 0;
+    // освобождаем ресурсы
+    cvReleaseImage(&image);
+    cvReleaseImage(&src);
+    // удаляем окно
+    cvDestroyWindow("original");
+    return 0;
 }
